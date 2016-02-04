d10f68a6a6d5c054dd8eb44a7dc42d25b1eb700e
chaoqiang <chaoqiang@staff.weibo.com>
2016-02-04 10:53:45 +0800

    mapreduce/src/main/java/io/uve/mapreduce/stat_pv_count.java

--- mapreduce/src/main/java/io/uve/mapreduce/stat_pv_count.java
+++ mapreduce/src/main/java/io/uve/mapreduce/stat_pv_count.java
@@ -2,17 +2,12 @@ package io.uve.mapreduce;
 
 import java.io.IOException;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class stat_pv_count {
+public class Stat_pv_count {
 	
 	public static class TokenizerMapper extends
 			Mapper<Object, Text, Text, IntWritable> {
@@ -42,19 +37,5 @@ public class stat_pv_count {
 			result.set(sum);
 			context.write(key, result);
 		}
-	}
-
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "stat_pv");
-		job.setJarByClass(stat_pv_count.class);
-		job.setMapperClass(TokenizerMapper.class);
-		job.setCombinerClass(IntSumReducer.class);
-		job.setReducerClass(IntSumReducer.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
