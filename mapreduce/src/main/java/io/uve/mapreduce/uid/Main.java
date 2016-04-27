8626862f531d439e619146cae10e2c4f6812e93a
chaoqiang <chaoqiang@staff.weibo.com>
2016-04-27 16:28:52 +0800

    add uid filter

---
+++ mapreduce/src/main/java/io/uve/mapreduce/uid/Main.java
@@ -0,0 +1,30 @@
+package io.uve.mapreduce.uid;
+
+import io.uve.mapreduce.uid.Stat_uid_filter.IntSumReducer;
+import io.uve.mapreduce.uid.Stat_uid_filter.TokenizerMapper;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "stats_uid");
+		job.setJarByClass(Stat_uid_filter.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setNumReduceTasks(0);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
---
+++ mapreduce/src/main/java/io/uve/mapreduce/uid/Stat_uid_filter.java
@@ -0,0 +1,37 @@
+package io.uve.mapreduce.uid;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class Stat_uid_filter {
+	
+	public static class TokenizerMapper extends
+			Mapper<Object, Text, Text, NullWritable> {
+//		private final static IntWritable one = new IntWritable(1);
+		private final static NullWritable empty = null;
+
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String msg = value.toString();
+			if (msg.contains("1111681197")) {
+				Text pv_key = new Text();
+				pv_key.set(msg);
+				context.write(pv_key, empty);
+			}
+		}
+	}
+
+	public static class IntSumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+//		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+		}
+	}
+}
